594d2f86ddcc08a08c957887a9dbe4209e0d5620
RAFAELA VALENTINA COSTA KAGE <rafaela.kage@etec.sp.gov.br>
2025-08-08 12:24:02 -0300

    terminei

---
+++ App.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import casual_dog from './src/assets/casual_dog.png';
+import Google from './src/assets/Google.png';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={casual_dog}
+        style={styles.casualDogImage}
+      />
+
+      <Text style={styles.title}>Ótimo dia!</Text>
+
+      <Text style={styles.subtitle}>Como deseja acessar?</Text>
+
+      <TouchableOpacity style={styles.tamanhoTelaButton}>
+        <Image
+          source={Google}
+          style={styles.buttonImage}
+        />
+        <Text style={styles.buttonText}>Como deseja acessar?</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.outlineButton}>
+        <Text style={styles.outlineButtonText}>Outras opções</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F5FCFF',
+    padding: 20,
+  },
+
+  casualDogImage: {
+    width: 300,
+    height: 250,
+    marginBottom: 20,
+  },
+
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+
+  subtitle: {
+    fontSize: 12,
+    marginBottom: 60,
+  },
+
+  tamanhoTelaButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'green',
+    padding: 15,
+    borderRadius: 10,
+    width: '100%', 
+    marginBottom: 10,
+  },
+
+  buttonImage: {
+    width: 30,
+    height: 30,
+    marginRight: 10,
+    backgroundColor: 'white',
+    borderRadius: 5,
+    marginRight: 10,
+  },
+
+  buttonText: {
+    color: 'white',
+    fontSize: 12,
+    fontWeight: 'bold',
+    marginLeft: 10,
+  },
+
+  outlineButton: {
+    borderColor: 'green',
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 15,
+    width: '100%', 
+    alignItems: 'center',
+  },
+
+  outlineButtonText: {
+    color: 'green',
+    fontSize: 12,
+    fontWeight: 'bold',
+  },
+});
