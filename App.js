cf0a1c21a27787b2743f5e4ff52e1c30dd5f930a
RAFAELA VALENTINA COSTA KAGE <rafaela.kage@etec.sp.gov.br>
2025-08-15 12:18:55 -0300

    screens, entrega?

--- App.js
+++ App.js
@@ -1,100 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
-import casual_dog from './src/assets/casual_dog.png';
-import Google from './src/assets/Google.png';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Tela1 from "./src/screens/Tela1";
+import Login from "./src/screens/Login";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Image
-        source={casual_dog}
-        style={styles.casualDogImage}
-      />
-
-      <Text style={styles.title}>Ótimo dia!</Text>
-
-      <Text style={styles.subtitle}>Como deseja acessar?</Text>
-
-      <TouchableOpacity style={styles.tamanhoTelaButton}>
-        <Image
-          source={Google}
-          style={styles.buttonImage}
-        />
-        <Text style={styles.buttonText}>Como deseja acessar?</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.outlineButton}>
-        <Text style={styles.outlineButtonText}>Outras opções</Text>
-      </TouchableOpacity>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Tela1">
+        <Stack.Screen name="Tela1" component={Tela1} />
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#F5FCFF',
-    padding: 20,
-  },
-
-  casualDogImage: {
-    width: 300,
-    height: 250,
-    marginBottom: 20,
-  },
-
-  title: {
-    fontSize: 35,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-
-  subtitle: {
-    fontSize: 12,
-    marginBottom: 60,
-  },
-
-  tamanhoTelaButton: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'green',
-    padding: 15,
-    borderRadius: 10,
-    width: '100%', 
-    marginBottom: 10,
-  },
-
-  buttonImage: {
-    width: 30,
-    height: 30,
-    marginRight: 10,
-    backgroundColor: 'white',
-    borderRadius: 5,
-    marginRight: 10,
-  },
-
-  buttonText: {
-    color: 'white',
-    fontSize: 12,
-    fontWeight: 'bold',
-    marginLeft: 10,
-  },
-
-  outlineButton: {
-    borderColor: 'green',
-    borderWidth: 2,
-    borderRadius: 10,
-    padding: 15,
-    width: '100%', 
-    alignItems: 'center',
-  },
-
-  outlineButtonText: {
-    color: 'green',
-    fontSize: 12,
-    fontWeight: 'bold',
-  },
-});
---
+++ src/screens/Login.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import googleImage from "./src/assets/Google.png";
+import 
